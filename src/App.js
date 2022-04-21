e06dc154ef9eb2a9c845f580502c981a73499e55
Fred Duguay <fredduguay@users.noreply.github.com>
2022-04-21 17:18:03 -0400

    NavLink & useParams examples

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink
+} from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
+// import Card from './components/shared/Card';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
 import AboutIconLink from './components/AboutIconLink';
+import Post from './components/Post';
 import AboutPage from './pages/AboutPage';
 
 import FeedbackData from './data/FeedbackData';
@@ -45,6 +52,18 @@ function App() {
           ></Route>
 
           <Route path='/about' element={<AboutPage />} />
+
+          <Route path='/post/:id/:name' element={<Post />} />
+
+          {/* #NavLink Example */}
+          {/* <Card>
+          <NavLink to='/' activeClassName='active'>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeClassName='active'>
+            About
+          </NavLink>
+          </Card> */}
         </Routes>
         <AboutIconLink />
       </div>
