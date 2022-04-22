2f5ac7b44769714fa7edd116748498e03b5aebaf
Fred Duguay <fredduguay@users.noreply.github.com>
2022-04-22 18:00:45 -0400

    Edit item (Function + Context)

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -22,15 +22,30 @@ export const FeedbackProvider = ({ children }) => {
     }
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false
+  });
+
+  //Add Feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
+  //Delete Feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
+  };
+
+  //Set item
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true
+    });
   };
 
   return (
@@ -38,7 +53,8 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         addFeedback,
-        deleteFeedback
+        deleteFeedback,
+        editFeedback
       }}
     >
       {children}
